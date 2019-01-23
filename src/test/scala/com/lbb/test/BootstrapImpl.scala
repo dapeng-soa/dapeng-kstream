cceb26820377bf3aa86ededa7b45a9b52499a275
bbliang <bbliang@today36524.com.cn>
2019-01-23 16:25:52 +0800

    Bootstrap test

---
+++ src/test/scala/com/lbb/test/BootstrapImpl.scala
@@ -0,0 +1,32 @@
+package com.lbb.test
+
+import java.time.Duration
+
+import com.dapeng.kstream.AbstractBootstrap
+import com.dapeng.kstream.pojo.GcInfo
+import com.google.gson.Gson
+import org.apache.kafka.streams.kstream.internals.DapengKStreamEnhancer._
+import org.apache.kafka.streams.scala.Serdes
+import org.apache.kafka.streams.scala.kstream.{Grouped, KStream, Materialized}
+
+class BootstrapImpl extends AbstractBootstrap{
+
+  override def build: Unit = {
+    println(" start to build topology......")
+    import builder._
+
+    topic("gcinfo")
+      .dapengFilter((k,v) => v.contains("Full Gc"))
+      .groupBy((k,v) => {
+        val matcher ="""(?<serviceName>^[a-zA-Z]+) (?<logtime>\d{2}-\d{2} \d{2}:\d{2}:\d{2} \d{3}) (?<threadPool>[^ ]+|Check idle connection Thread) (?<level>[^ ]+) \[(?<sessionTid>\w*)\] - (?<message>.*)""".r
+        val matcher(serviceName, _, _, _, _, _) = v
+        serviceName
+      })(Grouped.`with`(Serdes.String, Serdes.String))
+      .window(Duration.ofMinutes(1), 2)
+      .sendDingding("orderService",
+        (k,v: String) => {
+        val msg = s" Full Gc告警， service: ${k}, expect: 2, actual: ${String.valueOf(v)}"
+        (k, msg)
+      })
+  }
+}
---
+++ src/test/scala/com/lbb/test/BootstrapTest.scala
@@ -0,0 +1,7 @@
+package com.lbb.test
+
+object BootstrapTest extends App {
+
+  new BootstrapImpl().execute
+
+}
